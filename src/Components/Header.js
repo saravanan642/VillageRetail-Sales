2a20483ac20c988d3fce76c83644c48aacc1fe9e
saravanan642 <saravanansenthil605@gmail.com>
2026-01-04 20:29:33 +0530

    update   src/Components/Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,30 +1,38 @@
-import { FaStore, FaSeedling, FaShoppingCart, FaChartLine, FaUser } from "react-icons/fa";
 import icon1 from "../Assests/happy-farmer-agriculture-farming-symbol-vector-illustration-2D7RD94.jpg"
+import icon2 from "../Assests/vecteezy_extraordinary-artistic-urban-farmer-holding-fresh-produce_57721627.png"
 const Navbar = () => {
   return (
-    <nav className="flex justify-between items-center px-6 py-4  text-white">
+    <nav className="flex justify-between items-center    text-white">
 
       {/* Logo */}
-      <div className="flex items-center gap-3 text-xl font-bold bg-green-950 px-6 py-3 justify-around w-full" >
-        <div className=" flex"><img
-          src={icon1}
-          alt="logo"
-          width={80}
-          className=" rounded-2xl border-2 border-black p-1  "
-        />
+      <div className="flex items-center text-xl font-bold  py-3  w-full bg-green-950" >
+        <div className=" flex  ml-[80px]">
+          <img
+            src={icon1}
+            alt="logo"
+            width={80}
+            className=" rounded-2xl border-2 border-black p-1 bg-white  "
+          />
 
           <h1 className="text-white m-[20px]">
             VillageRetail Sales
           </h1></div>
 
-        <div className="   flex gap-8">
-          <a href="" className="text-white "> Home</a>
-          <a href="" className="text-white">Sales</a>
-          <a href="" className="text-white">Retailers</a>
-          <a href="" className="text-white">Sales</a>
+        <div className="   flex gap-[70px]  ml-[25%] mr-[5%]">
+          <a href="#" className="hover:text-green-500 transition">Home</a>
+          <a href="#" className="hover:text-green-500 transition">Sales</a>
+          <a href="#" className="hover:text-green-500 transition">Retailers</a>
+          <a href="#" className="hover:text-green-500 transition">Reports</a>
         </div>
-
-
+        <div  className=" bg-white p-1 border border-solid border-black p-1 bg-white ml-[2%]  rounded-xl flex  items-center">
+          <img
+            src={icon2}
+            alt="profile"
+            width={50}
+           
+          />
+          <a className=" text-gray-800  pr-[20px]   underline cursor-pointer hover:text-lime-600">Login</a>
+        </div>
       </div>
 
 
