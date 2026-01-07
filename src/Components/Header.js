1bb8d6afe64a82707316b5b69f2ce5c66b5c813e
saravanan642 <saravanansenthil605@gmail.com>
2026-01-07 21:13:24 +0530

    state    src/Components/Home.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -31,7 +31,7 @@ const Navbar = () => {
             <a className="hover:text-green-500 transition" href="Home">Home</a>
             <a className="hover:text-green-500 transition" href="About">About</a>
             <a className="hover:text-green-500 transition"  href="Retailers"> Retailers</a>
-            <a className="hover:text-green-500 transition" href="Contacts">services</a>
+            <a className="hover:text-green-500 transition" href="Contacts">Services</a>
           </div>
 
         
