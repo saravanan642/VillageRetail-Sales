78257874822baaa45655f2d2aeb3273e7e79960f
saravanan642 <saravanansenthil605@gmail.com>
2026-01-29 18:09:29 +0530

    header  update

--- src/Components/Sample.js
+++ src/Components/Sample.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 const Sample = () => {
-  return (
-    <header className="">
-      {/* <div className="flex items-center gap-8 justify-between bg-black text-white px-6 py-4
+    return (
+        <header className="">
+            {/* <div className="flex items-center gap-8 justify-between bg-black text-white px-6 py-4
       lg:text-green-950
       md:text-red-900
       sm:text-orange-950
@@ -22,11 +22,15 @@ const Sample = () => {
 
       </div> */}
 
-      <p className=" text-green-700 md:text-3xl sm:text-red-800 lg:text-indigo-500">welcome to React App using the Responsive</p>
+            <p className=" text-green-700 md:text-3xl sm:text-red-800 lg:text-indigo-500">welcome to React App using the Responsive</p>
+            <div className="flex sm:inline">
+                <h1 className="text-red-700 text-4xl lg:text-green-900 sm:text-blue-800 md:text-gray-950
+                                               sm:text-7xl">Saravanan</h1>
+                <p className="text-4xl sm:text-7xl">Pavinkumar</p>
 
-      <h1>Saravanan</h1>
-    </header>
-  );
+            </div>
+        </header>
+    );
 };
 
 export default Sample;
